f038213bbcf545d46f3d1f76df0ecd6c86c3e025
Sehui Park <mike.sayhi@gmail.com>
2014-05-27 15:06:44 -0400

    Add Simple Character Control

---
+++ Assets/Scripts/JimControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JimControl : MonoBehaviour {
+
+    int facing = 0;
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float input = Input.GetAxis("Horizontal");
+
+        facing = 0;
+        if (input > 0)
+            facing = 1;
+        else if (input < 0)
+            facing = -1;
+
+        anim.SetFloat("speed", Mathf.Abs(input));
+	}   
+}
