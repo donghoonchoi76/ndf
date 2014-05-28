5cd2750aa8c1a7b8915ac81acb98b376f1c71117
donghoonchoi76 <lynus76@gmail.com>
2014-05-27 23:45:06 -0400

    planet added.

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planet : MonoBehaviour {
+    public const int MAX_SLOT = 20;
+    public float fHP = 100.0f;
+    public float fElec = 300.0f;
+    public float fSpeed = 20.0f;
+
+	// Use this for initialization
+	void Start () {
+
+        iTween.RotateBy(gameObject, iTween.Hash("z", 90, "speed", fSpeed, "loopType", "loop", "easeType", "linear"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
