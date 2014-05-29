8daa0fb447baa2a7f2d06bb9a4d6b162710adc63
Sehui Park <mike.sayhi@gmail.com>
2014-05-29 10:54:11 -0400

    Change files

--- Assets/Scripts/JimControl.cs
+++ Assets/Scripts/JimControl.cs
@@ -1,26 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class JimControl : MonoBehaviour {
 
-    int facing = 0;
-    Animator anim;
+public class JimControl : MonoBehaviour
+{
+    enum STATE
+    {
+        IDLE,
+        WALK,
+        SHOOT,
+        WALK_SHOOT,
+        RECHARGE,
+        DAMAGE,
+    };
 
-	// Use this for initialization
-	void Start () {
-        anim = GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        float input = Input.GetAxis("Horizontal");
+    float m_fAngle = 0;
+    float m_fMoveSpeed = 2.0f;
 
-        facing = 0;
-        if (input > 0)
-            facing = 1;
-        else if (input < 0)
-            facing = -1;
+    // Use this for initialization
+    void Start()
+    {
+        //Animator anim;
+        //anim = GetComponent<Animator>();
+        //anim.SetFloat("speed", Mathf.Abs(input));
+    }
 
-        anim.SetFloat("speed", Mathf.Abs(input));
-	}   
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+    }
+
+    void Move()
+    {
+        int key = 0;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            key = -1;
+        if (Input.GetKey(KeyCode.RightArrow))
+            key = 1;
+
+        if (key != 0)
+        {
+            m_fAngle += -key * m_fMoveSpeed;
+            if (m_fAngle >= 360)
+                m_fAngle -= 360;
+            if (m_fAngle < 0)
+                m_fAngle += 360;
+        }
+        Quaternion qt = Quaternion.identity;
+        qt.eulerAngles = new Vector3(0, 0, m_fAngle);
+        transform.rotation = Quaternion.Slerp(transform.rotation, qt, 5.0f);
+    }
 }
